17c5c27a54a4ee4b06ec2a18fc135f6e566ea4fd
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-02-21 17:52:31 +0700

    update cors

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -5,7 +5,7 @@ import "github.com/rs/cors"
 // CORSMiddleware ...
 func CORSMiddleware() *cors.Cors {
 	return cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3001", "https://fptu.tech", "http://fptu.tech", "https://staging.fptu.tech", "http://staging.fptu.tech"},
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3001", "https://fptu.tech", "http://fptu.tech", "https://staging.fptu.tech", "http://staging.fptu.tech", "http://news.fptu.tech", "https://news.fptu.tech"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},

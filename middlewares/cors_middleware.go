b3449d4fe65900ea4954c8a35ad13812861b4151
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-04-06 21:33:01 +0700

    allow * luon, so deo gi

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -5,7 +5,7 @@ import "github.com/rs/cors"
 // CORSMiddleware ...
 func CORSMiddleware() *cors.Cors {
 	return cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3001", "https://fptu.tech", "http://fptu.tech", "http://fu.gosu.team", "https://fu.gosu.team"},
+		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
